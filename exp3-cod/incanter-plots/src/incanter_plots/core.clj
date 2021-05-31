9aee9240194405e4c3367a482e7d7225baa1389b
Branquinho <pedrogbranquinho@gmail.com>
2021-05-30 22:17:43 -0300

    Adição de plots e inserção de sicmutils no projeto

--- exp3-cod/incanter-plots/src/incanter_plots/core.clj
+++ exp3-cod/incanter-plots/src/incanter_plots/core.clj
@@ -1,7 +1,9 @@
 (ns incanter-plots.core
   (:require [incanter.core]
             [incanter.charts]
-            [incanter.stats]))
+            [incanter.stats]
+            [sicmutils.env :as env]))
+(env/bootstrap-repl!)
 
 (defn foo
   "I don't do a whole lot."
@@ -22,10 +24,27 @@
   (read-dataset "../../dados/FeAlB_Bin_sem núcleo.dat"
                 :header false
                 :delim \tab))
-
 (def time-data ($ :col0 sem-nucleo))
 (def B-data ($ :col1 sem-nucleo))
 (def Vs-data ($ :col3 sem-nucleo))
+
+(def com-nucleo
+  (read-dataset "../../dados/FeAlB_Bin_com núcleo.dat"
+                :header false
+                :delim \tab))
+(def time-data-com ($ :col0 com-nucleo))
+(def B-data-com ($ :col1 com-nucleo))
+(def Vs-data-com ($ :col3 com-nucleo))
+
+
+(def histerese
+  (read-dataset "../../dados/FeAlB-Histerese.dat"
+                :header false
+                :delim \tab))
+(def time-data-histerese ($ :col0 histerese))
+(def B-data-histerese ($ :col1 histerese))
+(def Vs-data-histerese ($ :col3 histerese))
+
 
 ;; (with-data sem-nucleo
 ;;    (view $data))
@@ -34,6 +53,36 @@
 (view (scatter-plot time-data B-data
                     :title "Experimento sem núcleo"
                     :x-label "Tempo (s)"
-                    :y-label "Campo Magnético"
+                    :y-label "Campo Magnético (B)"
                     :legend "B(t)"))
 
+(view (scatter-plot time-data Vs-data
+                    :title "Experimento sem núcleo"
+                    :x-label "Tempo (s)"
+                    :y-label "Voltagem (V)"
+                    :legend "Vs(t)"))
+
+(view (scatter-plot time-data-com B-data-com
+                    :title "Experimento com núcleo"
+                    :x-label "Tempo (s)"
+                    :y-label "Campo Magnético (B)"
+                    :legend "B(t)"))
+
+(view (scatter-plot time-data-com Vs-data-com
+                    :title "Experimento com núcleo"
+                    :x-label "Tempo (s)"
+                    :y-label "Voltagem (B)"
+                    :legend "Vs(t)"))
+
+
+(view (scatter-plot time-data-histerese B-data-histerese
+                    :title "Experimento sem núcleo"
+                    :x-label "Tempo (s)"
+                    :y-label "Campo Magnético (B)"
+                    :legend "B(t)"))
+
+(view (scatter-plot time-data-histerese Vs-data-histerese
+                    :title "Experimento sem núcleo"
+                    :x-label "Tempo (s)"
+                    :y-label "Voltagem (V)"
+                    :legend "Vs(t)"))
